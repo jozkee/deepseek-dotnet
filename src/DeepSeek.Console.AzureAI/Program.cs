5d9edbc298127fdae7b272d58313408d0afb67b0
Matt Soucoup <masoucou@microsoft.com>
2025-01-31 08:57:46 -0800

    Deepseek in azure

--- src/DeepSeek.Console.AzureAI/Program.cs
+++ src/DeepSeek.Console.AzureAI/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .AddUserSecrets<Program>();
+
+var configuration = builder.Build();
+
+// Make sure the Azure key is set in the user secrets
+string azureKey = configuration["AZURE_AI_KEY"] ??
+    throw new InvalidOperationException("Make sure to add AZURE_AI_KEY value to the user secrets.");
+
+// These variables are needed to access the GitHub Models
+AzureKeyCredential credential = new(azureKey);
+Uri modelEndpoint = new("GET THIS VALUE FROM YOUR DEPLOYMENT IN AZURE AI FOUNDRY");
+string modelName = "GET THIS VALUE FROM YOUR DEPLOYMENT IN AZURE AI FOUNDRY";
+
+
+IChatClient chatClient = new ChatCompletionsClient(modelEndpoint, credential)
+    .AsChatClient(modelName);
+
+string question = "If I have 3 apples and eat 2, how many bananas do I have?";
+var response = chatClient.CompleteStreamingAsync(question);
+
+Console.WriteLine($">>> User: {question}");
+Console.Write(">>>");
+Console.WriteLine(">>> DeepSeek (might be a while): ");
+
+await foreach (var item in response)
+{
+    Console.Write(item);
+}
