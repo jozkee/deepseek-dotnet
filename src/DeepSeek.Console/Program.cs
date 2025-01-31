68b0df055c6a84c7e90388c0351d7a5e8f8f0d32
codemillmatt <masoucou@microsoft.com>
2025-01-30 21:18:00 -0800

    Initial go

---
+++ src/DeepSeek.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations;
+
+var builder = new ConfigurationBuilder()
+    .AddUserSecrets<Program>();
+
+var configuration = builder.Build();
+
+// These variables are needed to access the GitHub Models
+string token = configuration["GH_TOKEN"] ?? 
+    throw new InvalidOperationException("Make sure to add GH_TOKEN value to the user secrets.");
+
+AzureKeyCredential credential = new(token);
+Uri modelEndpoint = new ("https://models.inference.ai.azure.com");
+string modelName = "DeepSeek-R1";
+
+
+IChatClient chatClient = new ChatCompletionsClient(modelEndpoint, credential)
+    .AsChatClient(modelName);
+
+string question = "If I have 3 apples and eat 2, how many bananas do I have?";
+var response = chatClient.CompleteStreamingAsync(question);
+
+Console.WriteLine($">>> User: {question}");
+Console.Write(">>>");
+Console.WriteLine(">>> DeepSeek (might be a while): ");
+
+await foreach (var item in response)
+{
+    Console.Write(item);
+}
